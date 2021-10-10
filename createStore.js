5c1b171a713eec6aa6472a92c338c097983dbeeb
eunsukimme <eunsu.dev@gmail.com>
2021-10-10 16:21:48 +0900

    feat: add handling middlewares

--- createStore.js
+++ createStore.js
@@ -1,20 +1,34 @@
-function createStore(reducer, initialState) {
+function createStore(reducer, initialState, middlewares = []) {
   let currentReducer = reducer;
   let currentState = initialState;
   const listeners = [];
 
+  function getState() {
+    return currentState;
+  }
+
+  const dispatch = (action) => {
+    currentState = currentReducer(currentState, action);
+    listeners.forEach((listener) => listener());
+    return action;
+  };
+
+  const lastDispatch = middlewares
+    .reverse()
+    .reduce(
+      (accDispatch, middleware) =>
+        middleware({ getState, dispatch })(accDispatch),
+      dispatch
+    );
+
+  function subscribe(newListener) {
+    listeners.push(newListener);
+  }
+
   return {
-    getState() {
-      return currentState;
-    },
-    dispatch(action) {
-      currentState = currentReducer(currentState, action);
-      listeners.forEach((listener) => listener()); // Note that we added this line!
-      return action;
-    },
-    subscribe(newListener) {
-      listeners.push(newListener);
-    },
+    getState,
+    dispatch: lastDispatch,
+    subscribe,
   };
 }
 
--- index.js
+++ index.js
@@ -11,8 +11,18 @@ function counterReducer(state = { value: 0 }, action) {
   }
 }
 
+const logger1 = (store) => (next) => (action) => {
+  console.log("LOGGER1: dispatching", action);
+  return next(action);
+};
+
+const logger2 = (store) => (next) => (action) => {
+  console.log("LOGGER2: dispatching", action);
+  return next(action);
+};
+
 // store API 는 { subscribe, dispatch, getState } 이다.
-let store = createStore(counterReducer);
+let store = createStore(counterReducer, undefined, [logger1, logger2]);
 
 // subscribe() 의 파라미터로 store의 state가 변경될 때 실행될 listener를 전달할 수 있다.
 store.subscribe(() => console.log(store.getState()));
